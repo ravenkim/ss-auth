afe2aab5305deaaf1329a21b72e71a9c8d120d6e
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-06-16 14:35:20 +0900

    로그인시 포트번호 추가

--- ss-auth-server/src/main/kotlin/ssauthserver/SsAuthServerApplication.kt
+++ ss-auth-server/src/main/kotlin/ssauthserver/SsAuthServerApplication.kt
@@ -2,10 +2,28 @@ package ssauthserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext
+import org.springframework.core.env.Environment
 
 @SpringBootApplication
 open class SsAuthServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<SsAuthServerApplication>(*args)
+    val applicationContext = runApplication<SsAuthServerApplication>(*args)
+
+    val environment = applicationContext.getBean(Environment::class.java)
+    val activeProfiles = environment.activeProfiles
+    val isLocal = activeProfiles.contains("local")
+
+    if (isLocal) {
+        val webServerAppCtxt = applicationContext as ServletWebServerApplicationContext
+        val port = webServerAppCtxt.webServer.port
+        val applicationName = environment.getProperty("spring.application.name")
+
+        println("\n----------------------------------------------------------")
+        println("Application '$applicationName' is running!")
+        println("Access URL:")
+        println("Local: http://localhost:$port")
+        println("----------------------------------------------------------\n")
+    }
 }
