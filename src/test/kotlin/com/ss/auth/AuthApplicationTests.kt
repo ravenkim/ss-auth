925769b844460a5c3b13b394730a3af717e01b60
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-08-28 14:36:16 +0900

    초기화

--- src/test/kotlin/com/ss/auth/AuthApplicationTests.kt
+++ src/test/kotlin/com/ss/auth/AuthApplicationTests.kt
@@ -1,13 +1,13 @@
-package ssauthserver
+package com.ss.auth
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SsAuthServerApplicationTests {
+class AuthApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+	@Test
+	fun contextLoads() {
+	}
 
 }
